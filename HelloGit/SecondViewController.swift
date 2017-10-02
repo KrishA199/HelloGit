c087be1ebebdb873efa92f4b9ed5ad4d1a6f104a
student <s528751@ch140006m.local>
2017-10-02 12:19:22 -0500

    Initial Commit

---
+++ HelloGit/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  HelloGit
+//
+//  Created by Mucherla,Veera Kishon Kumar on 10/2/17.
+//  Copyright © 2017 Mucherla,Veera Kishon Kumar. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
